bd0440fdccae1bbfd8b9ba069a3ddbcfe791f5a7
adhitya yosua sanjaya <adityasanjaya@sicepat.com>
2022-08-01 21:27:54 +0700

    feat: change app name

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 
 export default {
   expo: {
-    name: "mobile-traces",
-    slug: "mobile-traces",
+    name: "pocket-trace",
+    slug: "pocket-trace",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
