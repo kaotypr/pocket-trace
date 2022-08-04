1a64276d7a67c5e70b656fa51f8b872af5dbfcaf
adhitya yosua sanjaya <adityasanjaya@sicepat.com>
2022-08-04 18:49:21 +0700

    feat: wrap app in auth provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,19 @@
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import ErrorBoundary from "@components/ErrorBoundary";
+import { AuthProvider } from "@contexts/authContext";
 import RootNavigation from "@navigations/root";
 
 export default function App() {
   return (
     <ErrorBoundary>
-      <RootNavigation />
+      <AuthProvider>
+        <SafeAreaProvider>
+          <RootNavigation />
+        </SafeAreaProvider>
+      </AuthProvider>
+      <StatusBar barStyle="dark-content" />
     </ErrorBoundary>
   );
 }
