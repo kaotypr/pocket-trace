1cf1f9984b365a7e7ebd10ab50d39c4740de474b
adhitya yosua sanjaya <adityasanjaya@sicepat.com>
2022-08-04 18:46:02 +0700

    chore: install & mock async-storage

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,7 @@
 import "react-native-gesture-handler/jestSetup";
+import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+
+jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
