788607c6d3c60fa22acf0867cedd6acb7d63936b
adhitya yosua sanjaya <adityasanjaya@sicepat.com>
2022-08-04 18:56:10 +0700

    feat: back header component
    component to show back button in a screen

---
+++ src/components/Headers/BackHeaders.tsx
@@ -0,0 +1,23 @@
+import { Feather } from "@expo/vector-icons";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
+import { TouchableOpacity, View } from "react-native";
+
+import { COLOR_LIGHT } from "@services/constants/color";
+
+const BackHeaders = ({ navigation }: Pick<NativeStackHeaderProps, "navigation">) => {
+  if (!navigation.canGoBack()) {
+    return null;
+  }
+
+  return (
+    <View style={{ zIndex: 1 }}>
+      <TouchableOpacity
+        onPress={() => navigation.goBack()}
+        style={{ position: "absolute", top: 0, left: 20 }}>
+        <Feather name="arrow-left-circle" color={COLOR_LIGHT.PRIMARY} size={35} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BackHeaders;
