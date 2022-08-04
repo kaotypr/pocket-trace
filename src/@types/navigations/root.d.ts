85cf017f8a76b65bd4e2fadc9e2e97ddbab45343
adhitya yosua sanjaya <adityasanjaya@sicepat.com>
2022-08-04 18:51:52 +0700

    feat: auth scene screen names

--- src/@types/navigations/root.d.ts
+++ src/@types/navigations/root.d.ts
@@ -3,6 +3,7 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SCREEN_NAMES } from "@services/constants/screen";
 
 type RootStackParamList = {
+  [SCREEN_NAMES.AUTH_SCENE]: undefined;
   [SCREEN_NAMES.HOME]: undefined;
   [SCREEN_NAMES.ABOUT]: undefined;
 };
