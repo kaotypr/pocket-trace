ad302a46129faf3fa40dceace3d4c3ebb8ef1837
adhitya yosua sanjaya <adityasanjaya@sicepat.com>
2022-08-04 18:54:17 +0700

    fix: app icon source use ES6 import

---
+++ src/@types/images.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
